9588352c549904e49a48b8ef88acd55bb23ede89
amannkumar <113698292+amannkumar@users.noreply.github.com>
2022-10-12 15:37:35 +0530

    Update main.dart
    changed the title

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Bloc Page'),
+      home: const MyHomePage(title: 'Flutter Counter Bloc Page'),
     );
   }
 }
